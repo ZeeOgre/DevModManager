9ed0c4ea248b20f0a5209894da14829d495a9134
ZeeOgre <177573496+ZeeOgre@users.noreply.github.com>
2026-02-18 15:31:08 -0600

    yaml library and wrapper, "yamlwrite" for debug until we wire up database

---
+++ DMM.Core/IO/Yaml.cs
@@ -0,0 +1,24 @@
+﻿using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace DMM.Core.IO
+{
+    public static class Yaml
+    {
+        public static readonly ISerializer Serializer =
+            new SerializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .ConfigureDefaultValuesHandling(DefaultValuesHandling.OmitNull)
+                .Build();
+
+        public static readonly IDeserializer Deserializer =
+            new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .IgnoreUnmatchedProperties()
+                .Build();
+
+        public static string Dump<T>(T value) => Serializer.Serialize(value);
+
+        public static T Load<T>(string yaml) => Deserializer.Deserialize<T>(yaml);
+    }
+}
